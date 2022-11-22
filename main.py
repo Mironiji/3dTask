a2c8ded93a8789fba53d19abfd9ce4fd53db3069
Admin <mkm@yandex.ru>
2022-11-22 21:40:07 +0300

    working var

--- main.py
+++ main.py
@@ -4,10 +4,40 @@ from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from random import randint
 
-class Example(QMainWindow):
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(915, 710)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(340, 250, 171, 101))
+        self.pushButton.setObjectName("pushButton")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 915, 26))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "je"))
+
+class Example(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.pushButton.clicked.connect(self.run)
         self.flag = False
 
@@ -19,7 +49,7 @@ class Example(QMainWindow):
         qp = QPainter()
         qp.begin(self)
         if self.flag:
-            qp.setBrush(QColor(*[250, 250, 0]))
+            qp.setBrush(QColor(*[randint(0, 255) for _ in range(3)]))
             size = randint(10, 200)
             x, y = randint(10, 500), randint(10, 500)
             qp.drawEllipse(x, y, size, size)
